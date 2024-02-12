a826db4e74a1bdfc5fe2b478695abd3982e354fe
ArmanKhachatryan2001 <armkhachatryan453@gmail.com>
2024-02-12 19:28:04 +0400

    Update main.cpp

--- visualCompiler/main.cpp
+++ visualCompiler/main.cpp
@@ -1,1 +1,12 @@
-
+int main(int argc, char* argv[])
+{
+    std::cout << "hello";
+    std::cout << std::setw(10) << "world" << std::endl;
+    if (argc < 2) {
+        std::cout << "Enter your text file. example ./a.out text.txt" << std::endl;
+        return 1;
+    }
+    Types obj(argv[1]);
+    obj.run();
+    return 0;
+}

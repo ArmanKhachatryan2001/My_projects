b13510afd06b8d91314e999682bf83f016f9abc1
ArmanKhachatryan2001 <armkhachatryan453@gmail.com>
2024-02-12 21:02:48 +0400

    Update main.cpp

--- visualCompiler/main.cpp
+++ visualCompiler/main.cpp
@@ -1,7 +1,5 @@
 int main(int argc, char* argv[])
 {
-    std::cout << "hello";
-    std::cout << std::setw(10) << "world" << std::endl;
     if (argc < 2) {
         std::cout << "Enter your text file. example ./a.out text.txt" << std::endl;
         return 1;
